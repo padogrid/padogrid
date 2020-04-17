16782c98677ed7a541908c58b8f6dda7c5a1c23c
dpark <dspark@netcrest.com>
2020-04-17 18:41:11 -0400

    Fixed addon migration leftovers

--- padogrid-deployment/src/main/resources/geode/k8s/kustom/bin_sh/.addonenv.sh
+++ padogrid-deployment/src/main/resources/geode/k8s/kustom/bin_sh/.addonenv.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 APP_DIR="$(dirname "$SCRIPT_DIR")"
 APPS_DIR="$(dirname "$APP_DIR")"
-BASE_DIR=$PADOHUB_HOME
+BASE_DIR=$PADOGRID_HOME/$PRODUCT
 pushd  $BASE_DIR/bin_sh > /dev/null 2>&1
 . $BASE_DIR/bin_sh/.addonenv.sh
 popd > /dev/null 2>&1
