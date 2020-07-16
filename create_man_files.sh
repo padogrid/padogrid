1d643ba5943f2875d550a02e03a86ca3143e3e97
dpark <dspark@netcrest.com>
2020-07-15 20:04:45 -0400

    Added bin_sh/tools man pages

--- create_man_files.sh
+++ create_man_files.sh
@@ -76,13 +76,18 @@ for PRODUCT in $PRODUCTS; do
    fi
    COMMANDS=""
    for i in bin_sh/*; do
-      if [ "$i" == "bin_sh/cp_sub" ] || [ "$i" == "bin_sh/setenv.sh" ]; then
+      if [ "$i" == "bin_sh/cp_sub" ] || [ "$i" == "bin_sh/tools" ] || [ "$i" == "bin_sh/setenv.sh" ]; then
          continue;
       fi
       COMMANDS="$COMMANDS $i"
    done
    if [ -d "bin_sh/cp_sub" ]; then
       for i in bin_sh/cp_sub/*; do
+         COMMANDS="$COMMANDS $i"
+      done
+   fi
+   if [ -d "bin_sh/tools" ]; then
+      for i in bin_sh/tools/*; do
          COMMANDS="$COMMANDS $i"
       done
    fi
