0b0893df7858a7207a0e87bbad9a8b9cf3b39f6e
dpark <dspark@netcrest.com>
2020-08-20 09:46:07 -0400

    Added support for ReplicatedMap in Debezium plugin

--- hazelcast-addon-core-4/src/main/java/org/hazelcast/addon/kafka/debezium/DebeziumKafkaAvroSinkConnector.java
+++ hazelcast-addon-core-4/src/main/java/org/hazelcast/addon/kafka/debezium/DebeziumKafkaAvroSinkConnector.java
@@ -36,12 +36,16 @@ public class DebeziumKafkaAvroSinkConnector extends SinkConnector {
 	public static final String VALUE_COLUMN_NAMES_CONFIG = "value.column.names";
 	public static final String VALUE_FIELD_NAMES_CONFIG = "value.field.names";
 	public static final String HAZELCAST_CLIENT_CONFIG_FILE_CONFIG = "hazelcast.client.config";
+	public static final String PARTITION_AWARE_INDEXES_CONFIG = "key.partitionAware.indexes";
+	public static final String MAP_REPLICATED_MAP_ENABLED = "map.replicatedMap.enabled";
 
 	private static final ConfigDef CONFIG_DEF = new ConfigDef()
 			.define(HAZELCAST_CLIENT_CONFIG_FILE_CONFIG, Type.STRING, "/hazelcast-addon/etc/hazelcast-client.xml",
 					Importance.MEDIUM, "Hazelcast client configuration file path.")
 			.define(MAP_CONFIG, Type.STRING, "map", Importance.HIGH,
-					"Destination IMap name. If not specified, then 'map' is assigned.")
+					"Destination map name. If not specified, then 'map' is assigned.")
+			.define(MAP_REPLICATED_MAP_ENABLED, Type.BOOLEAN, false, Importance.MEDIUM,
+					"ReplicatedMap flag. If true, then data is stored in ReplicatedMap, otherwise, IMap.")
 			.define(DEBUG_ENABLED, Type.BOOLEAN, false, Importance.LOW,
 					"Debug flag. If true, then debug information is printed.")
 			.define(SMT_ENABLED, Type.BOOLEAN, true, Importance.HIGH,
@@ -51,7 +55,7 @@ public class DebeziumKafkaAvroSinkConnector extends SinkConnector {
 			.define(HAZELCAST_ENABLED, Type.BOOLEAN, true, Importance.HIGH,
 					"Hazelcast flag. If true, then Hazelcast is enabled and it makes connection to Hazelcast. "
 					+ "If false, Hazelcast is ignored and only displays consumed data.")
-			.define(AVRO_DEEP_COPY_ENABLED, Type.BOOLEAN, true, Importance.MEDIUM,
+			.define(AVRO_DEEP_COPY_ENABLED, Type.BOOLEAN, false, Importance.MEDIUM,
 					"AVRO deep copy flag. If true, then the payload is deep-copied from Struct to Avro. Note "
 					+ "that deep-copy copies the entire payload to the Avro object and therefore filtering "
 					+ "is not supported. If false, the payload is shallow-copied from Struct to data object. "
@@ -62,7 +66,7 @@ public class DebeziumKafkaAvroSinkConnector extends SinkConnector {
 							+ "If not specified but the key column names are specified, then the column values are concatenated with the delimiter '.'. "
 							+ "If the key column names are also not specified, then the Kafka key is used.")
 			.define(KEY_COLUMN_NAMES_CONFIG, Type.STRING, null, Importance.HIGH,
-					"Comma separated key column names. An ordered list of table column names to be mapped to the key object field (setter) names.")
+					"Comma separated key column names. An ordered list of table column names to be mapped to the key object field (setter) names. If key column names are not defined, i.e., null, then a random UUID is assigned to the key instead.")
 			.define(KEY_FIELD_NAMES_CONFIG, Type.STRING, null, Importance.HIGH,
 					"Comma separated key object field (setter) names. An ordered list of key object field (setter) names to be mapped to the table column names.")
 			.define(VALUE_CLASS_NAME_CONFIG, Type.STRING, null, Importance.HIGH,
@@ -71,10 +75,14 @@ public class DebeziumKafkaAvroSinkConnector extends SinkConnector {
 			.define(VALUE_COLUMN_NAMES_CONFIG, Type.STRING, null, Importance.HIGH,
 					"Comma separated value column names. An ordered list of table column names to be mapped to the value object field (setter) names.")
 			.define(VALUE_FIELD_NAMES_CONFIG, Type.STRING, null, Importance.HIGH,
-					"Comma separated value object field (setter) names. An ordered list of value object field (setter) names to be mapped to the table column names.");
-
+					"Comma separated value object field (setter) names. An ordered list of value object field (setter) names to be mapped to the table column names.")
+			.define(PARTITION_AWARE_INDEXES_CONFIG, Type.STRING, null, Importance.MEDIUM,
+					"Comma separated indexes of the value fields for creating the partition ID and co-locating data. The first index of the value field list is 0."
+					+ " The Hazelcast map must be configured with ParititionStrategy set to StringPartitioningStrategy or StringAndPartitionAwarePartitioningStrategy.");
+			
 	private String hazelcastClientConfigFile;
 	private String mapName;
+	private boolean isReplicatedMapEnabled = false;
 	private boolean isDebugEnabled = false;
 	private boolean isSmtEnabled = true;
 	private boolean isDeleteEnabled = true;
@@ -86,6 +94,7 @@ public class DebeziumKafkaAvroSinkConnector extends SinkConnector {
 	private String valueClassName;
 	private String valueColumnNames;
 	private String valueFieldNames;
+	private String partitionAwareIndexes;
 
 	@Override
 	public String version() {
@@ -97,6 +106,8 @@ public class DebeziumKafkaAvroSinkConnector extends SinkConnector {
 		AbstractConfig parsedConfig = new AbstractConfig(CONFIG_DEF, props);
 		hazelcastClientConfigFile = parsedConfig.getString(HAZELCAST_CLIENT_CONFIG_FILE_CONFIG);
 		mapName = parsedConfig.getString(MAP_CONFIG);
+		String isReplicatedMapEnabledStr = props.get(MAP_REPLICATED_MAP_ENABLED);
+		isReplicatedMapEnabled = isReplicatedMapEnabledStr != null && isReplicatedMapEnabledStr.equalsIgnoreCase("true") ? true : isReplicatedMapEnabled;
 		String isDebugStr = props.get(DEBUG_ENABLED);
 		isDebugEnabled = isDebugStr != null && isDebugStr.equalsIgnoreCase("true") ? true : isDebugEnabled;
 		String isSmtStr = props.get(SMT_ENABLED);
@@ -113,6 +124,7 @@ public class DebeziumKafkaAvroSinkConnector extends SinkConnector {
 		valueClassName = props.get(VALUE_CLASS_NAME_CONFIG);
 		valueColumnNames = props.get(VALUE_COLUMN_NAMES_CONFIG);
 		valueFieldNames = props.get(VALUE_FIELD_NAMES_CONFIG);
+		partitionAwareIndexes = props.get(PARTITION_AWARE_INDEXES_CONFIG);
 
 		System.out.println(
 				"connector: ====================================================================================");
@@ -127,6 +139,7 @@ public class DebeziumKafkaAvroSinkConnector extends SinkConnector {
 
 		System.out.println(props);
 		System.out.println("connector: mapName = " + mapName);
+		System.out.println("connector: isReplicatedMapEnabled = " + isReplicatedMapEnabled);
 		System.out.println("connector: isSmtEnabled = " + isSmtEnabled);
 		System.out.println("connector: isDeleteEnabled = " + isDeleteEnabled);
 		System.out.println("connector: isHazelcastEnabled = " + isHazelcastEnabled);
@@ -152,6 +165,7 @@ public class DebeziumKafkaAvroSinkConnector extends SinkConnector {
 			if (mapName != null) {
 				config.put(MAP_CONFIG, mapName);
 			}
+			config.put(MAP_REPLICATED_MAP_ENABLED, Boolean.toString(isReplicatedMapEnabled));
 			config.put(DEBUG_ENABLED, Boolean.toString(isDebugEnabled));
 			config.put(SMT_ENABLED, Boolean.toString(isSmtEnabled));
 			config.put(DELETE_ENABLED, Boolean.toString(isDeleteEnabled));
@@ -174,6 +188,9 @@ public class DebeziumKafkaAvroSinkConnector extends SinkConnector {
 			}
 			if (valueFieldNames != null) {
 				config.put(VALUE_FIELD_NAMES_CONFIG, valueFieldNames);
+			}
+			if (partitionAwareIndexes != null) {
+				config.put(PARTITION_AWARE_INDEXES_CONFIG, partitionAwareIndexes);
 			}
 			configs.add(config);
 		}
