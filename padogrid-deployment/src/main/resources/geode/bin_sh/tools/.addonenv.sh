67f37d19ced5d1d9f2dd9d1277655722d8c985ce
Dae Song Park <dspark@netcrest.com>
2022-09-30 20:40:29 -0400

    Fixed setenv.sh sourcing issue in geode/bin_sh/tools

--- padogrid-deployment/src/main/resources/geode/bin_sh/tools/.addonenv.sh
+++ padogrid-deployment/src/main/resources/geode/bin_sh/tools/.addonenv.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
-BIN_DIR="$(dirname "$SCRIPT_DIR")"
+tools_SCRIPT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+
+BIN_DIR="$(dirname "$tools_SCRIPT_DIR")"
 pushd $BIN_DIR > /dev/null 2>&1
 . $BIN_DIR/.addonenv.sh -script_dir $BIN_DIR $*
 popd > /dev/null 2>&1
-SCRIPT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
-. $SCRIPT_DIR/setenv.sh
+SCRIPT_DIR=$tools_SCRIPT_DIR
+if [ -f $SCRIPT_DIR/setenv.sh ]; then
+   . $SCRIPT_DIR/setenv.sh
+fi
