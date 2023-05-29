0e3ae67b457a49a244ee4cdb4065676664967348
Dae Song Park <dspark@netcrest.com>
2023-05-29 17:25:48 -0400

    Fixed concurrent mod and supressed jvm warning for paho

--- padogrid-mqtt/src/test/java/padogrid/mqtt/test/cluster/console/TopicSubscriber.java
+++ padogrid-mqtt/src/test/java/padogrid/mqtt/test/cluster/console/TopicSubscriber.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package padogrid.mqtt.addon.test.cluster.console;
+package padogrid.mqtt.test.cluster.console;
 
 import java.io.IOException;
 
