b699490cc4dc4aa7a3a643c476dc6be9730c3d91
javapark1 <javapark7@gmail.com>
2024-03-01 12:26:35 -0500

    Added slf4j and log4j to tools classpath

--- padogrid-deployment/src/main/resources/tools/bin_sh/.addonenv.sh
+++ padogrid-deployment/src/main/resources/tools/bin_sh/.addonenv.sh
@@ -5,3 +5,12 @@ BIN_DIR="$(dirname "$SCRIPT_DIR")"
 pushd $BIN_DIR > /dev/null 2>&1
 . $BIN_DIR/.addonenv.sh -script_dir $BIN_DIR $*
 popd > /dev/null 2>&1
+
+# Add log4j which was taken out by the main .addonenv.sh to prevent jar conflicts
+# for running clusters. The commands in tools do not have cluster dependencies.
+for i in $PADOGRID_HOME/lib/*; do
+  if [[ "$i" == *"slf4j"* ]] || [[ "$i" == *"log4j"* ]]; then
+     CLASSPATH="$CLASSPATH:$i"
+  fi
+done
+
